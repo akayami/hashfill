bc667f83fbde19fba8f5c507cc82aefe7b1a641b
Tomasz Rakowski <akayami@gmail.com>
2024-01-16 17:31:40 -0500

    Initial attempt at reworking the library.

--- predicates.go
+++ predicates.go
@@ -2,7 +2,8 @@ package hashfill
 
 import (
 	"github.com/mmcloughlin/geohash"
-	"github.com/paulsmith/gogeos/geos"
+	"github.com/paulmach/orb"
+	"github.com/paulmach/orb/planar"
 	geom "github.com/twpayne/go-geom"
 )
 
@@ -33,55 +34,128 @@ func (f intersectsFunc) Intersects(p *geom.Polygon, hash string) (bool, error) {
 // Intersects tests if the geofence contains the hash by doing a geos intersection.
 var Intersects = intersectsFunc(func(geofence *geom.Polygon, hash string) (bool, error) {
 	hashGeo := hashToGeometry(hash)
-	fence := polygonToGeometry(geofence)
-	return fence.Intersects(hashGeo)
+	fence := ConvertGeomPolygonToOrbPolygon(geofence)
+	return polygonIntersectsPolygon(fence, hashGeo), nil
 })
 
 // Contains tests if the geofence contains the hash by doing a geos contains.
 var Contains = containsFunc(func(geofence *geom.Polygon, hash string) (bool, error) {
 	hashGeo := hashToGeometry(hash)
-	fence := polygonToGeometry(geofence)
-	return fence.Contains(hashGeo)
+	fence := ConvertGeomPolygonToOrbPolygon(geofence)
+	return polygonContainsPolygon(hashGeo, fence), nil
 })
 
-func geomToGeosCoord(coord geom.Coord) geos.Coord {
-	return geos.Coord{
-		X: coord.X(),
-		Y: coord.Y(),
-	}
-}
+//func geomToGeosCoord(coord geom.Coord) geos.Coord {
+//	return geos.Coord{
+//		X: coord.X(),
+//		Y: coord.Y(),
+//	}
+//}
 
-func geomToGeosCoords(coords []geom.Coord) []geos.Coord {
-	out := make([]geos.Coord, len(coords))
-	for i := 0; i < len(coords); i++ {
-		out[i] = geomToGeosCoord(coords[i])
-	}
-	return out
-}
+//func geomToGeosCoords(coords []geom.Coord) []geos.Coord {
+//	out := make([]geos.Coord, len(coords))
+//	for i := 0; i < len(coords); i++ {
+//		out[i] = geomToGeosCoord(coords[i])
+//	}
+//	return out
+//}
 
 // hashToGeometry converts a a geohash to a geos polygon by taking its bounding box.
-func hashToGeometry(hash string) *geos.Geometry {
+func hashToGeometry(hash string) orb.Polygon {
 	bounds := geohash.BoundingBox(hash)
-	return geos.Must(geos.NewPolygon([]geos.Coord{
-		geos.NewCoord(bounds.MinLng, bounds.MinLat),
-		geos.NewCoord(bounds.MinLng, bounds.MaxLat),
-		geos.NewCoord(bounds.MaxLng, bounds.MaxLat),
-		geos.NewCoord(bounds.MaxLng, bounds.MinLat),
-		geos.NewCoord(bounds.MinLng, bounds.MinLat),
-	}))
+	return orb.Polygon{
+		{
+			{bounds.MinLng, bounds.MinLat},
+			{bounds.MinLng, bounds.MaxLat},
+			{bounds.MaxLng, bounds.MaxLat},
+			{bounds.MaxLng, bounds.MinLat},
+			{bounds.MinLng, bounds.MinLat},
+		},
+	}
 }
 
-func polygonToGeometry(geofence *geom.Polygon) *geos.Geometry {
-	// Convert the outer shell to geos format.
-	shell := geofence.LinearRing(0).Coords()
-	shellGeos := geomToGeosCoords(shell)
+//func polygonToGeometry(geofence *geom.Polygon) *geos.Geometry {
+//	// Convert the outer shell to geos format.
+//	shell := geofence.LinearRing(0).Coords()
+//	shellGeos := geomToGeosCoords(shell)
+//
+//	// Convert each hole to geos format.
+//	numHoles := geofence.NumLinearRings() - 1
+//	holes := make([][]geos.Coord, numHoles)
+//	for i := 0; i < numHoles; i++ {
+//		holes[i] = geomToGeosCoords(geofence.LinearRing(i).Coords())
+//	}
+//
+//	return geos.Must(geos.NewPolygon(shellGeos, holes...))
+//}
 
-	// Convert each hole to geos format.
-	numHoles := geofence.NumLinearRings() - 1
-	holes := make([][]geos.Coord, numHoles)
-	for i := 0; i < numHoles; i++ {
-		holes[i] = geomToGeosCoords(geofence.LinearRing(i).Coords())
+// ConvertGeomPolygonToOrbPolygon converts a geom.Polygon to an orb.Polygon
+func ConvertGeomPolygonToOrbPolygon(gPolygon *geom.Polygon) orb.Polygon {
+	numRings := gPolygon.NumLinearRings()
+	oPolygon := make(orb.Polygon, numRings)
+
+	for i := 0; i < numRings; i++ {
+		ring := gPolygon.LinearRing(i)
+		coords := ring.FlatCoords()
+		numCoords := len(coords) / 2 // Divide by 2 as each coordinate is a pair of values
+		oRing := make(orb.Ring, numCoords)
+
+		for j := 0; j < numCoords; j++ {
+			oRing[j] = orb.Point{coords[2*j], coords[2*j+1]}
+		}
+
+		oPolygon[i] = oRing
 	}
 
-	return geos.Must(geos.NewPolygon(shellGeos, holes...))
+	return oPolygon
+}
+
+func polygonContainsPolygon(outer, inner orb.Polygon) bool {
+	for _, ring := range inner {
+		for _, point := range ring {
+			if !planar.PolygonContains(outer, point) {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func polygonIntersectsPolygon(outer, inner orb.Polygon) bool {
+	if !inner.Bound().Intersects(outer.Bound()) {
+		return false
+	}
+	if !outer.Bound().Intersects(inner.Bound()) {
+		return false
+	}
+
+	return polygonsIntersect(outer, inner)
+}
+
+func lineIntersects(a1, a2, b1, b2 orb.Point) bool {
+	// Based on the "Line segment intersection" algorithm
+	det := (a2[0]-a1[0])*(b2[1]-b1[1]) - (b2[0]-b1[0])*(a2[1]-a1[1])
+	if det == 0 {
+		return false // Collinear or parallel
+	}
+	lambda := ((b2[1]-b1[1])*(b2[0]-a1[0]) + (b1[0]-b2[0])*(b2[1]-a1[1])) / det
+	gamma := ((a1[1]-a2[1])*(b2[0]-a1[0]) + (a2[0]-a1[0])*(b2[1]-a1[1])) / det
+	return (0 < lambda && lambda < 1) && (0 < gamma && gamma < 1)
+}
+
+func polygonsIntersect(poly1, poly2 orb.Polygon) bool {
+	for _, ring1 := range poly1 {
+		for i := 0; i < len(ring1)-1; i++ {
+			edge1Start, edge1End := ring1[i], ring1[i+1]
+			for _, ring2 := range poly2 {
+				for j := 0; j < len(ring2)-1; j++ {
+					edge2Start, edge2End := ring2[j], ring2[j+1]
+					if lineIntersects(edge1Start, edge1End, edge2Start, edge2End) {
+						return true
+					}
+				}
+			}
+		}
+	}
+	return false
 }
